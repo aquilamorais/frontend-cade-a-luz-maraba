6938929efb6d218a5b540fe6534331b0ea95374e
Aquila-M <atemoraiscosta88@gmail.com>
2025-12-03 15:39:25 -0300

    fix api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = (import.meta as any).env.VITE_API_URL;
 
 export const api = axios.create({
     baseURL: API_URL,
